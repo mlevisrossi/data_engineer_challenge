0a91ec03fca02af325c3073874dbe6585aea0501
mlevisrossi <mlevisrossi@gmail.com>
2022-01-12 23:34:48 -0300

    postgres files added

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# data_engineer_challenge
+## Requirements
+
+### Instalation
+- Python 3.8
+- Docker
+  
+### Run the following commands:
+- pip install requests
+- pip install psycopg2
+- pip install dbt-postgres
+- dbt init
+
+### Modify the file profiles.yml
+
+
+- 
+
+
+Try running the following commands:
+- 
+
+
+- dbt run
+- dbt test
+
+
+### Resources:
+- Learn more about dbt [in the docs](https://docs.getdbt.com/docs/introduction)
+- Check out [Discourse](https://discourse.getdbt.com/) for commonly asked questions and answers
+- Join the [chat](https://community.getdbt.com/) on Slack for live discussions and support
+- Find [dbt events](https://events.getdbt.com) near you
+- Check out [the blog](https://blog.getdbt.com/) for the latest news on dbt's development and best practices
---
+++ de_challenge/extract_load.py
@@ -0,0 +1,35 @@
+import json
+import psycopg2
+import requests
+import pandas as pd
+from sqlalchemy import create_engine
+
+# connect to database
+engine = create_engine('postgresql+psycopg2://mlevisrossi:mlevisrossi@localhost:5432/postgres')
+connection = engine.raw_connection()
+
+# create a cursor
+cur = connection.cursor()
+
+# read sql file
+f = open('DDL_incidents.sql', 'r')
+sql_create = f.read()
+f.close()
+
+# execute create table statement
+cur.execute(sql_create)
+
+# data extraction from API and creation of pandas dataframe
+URL = 'https://data.sfgov.org/resource/wr8u-xric.json'
+df = pd.read_json(URL)
+
+# replace point type for text
+df['point'] = str(df['point'])
+
+# insert in database
+print('antes de insertar en bd')
+df.to_sql('incidents', engine, schema='public', if_exists='replace', index=False)
+print('done')
+#close connection
+cur.close()
+connection.close()
